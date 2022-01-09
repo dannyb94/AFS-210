c0469a7a1cac3f9109ceb0414a91a64ab4218e54
dannyb94 <danielle.ussery@bryanuniversity.edu>
2022-01-09 00:18:21 -0800

    week5

--- week5/searching/binarysearch.py
+++ week5/searching/binarysearch.py
@@ -1,19 +1,39 @@
 # Write the code to perform a binary search on a sorted list.  The function should accept a sorted list and the value to search for as inputs.
 # The function should return either True or False depending if the provided value was found in the sorted list.
 
-# Sample Input:
-# [7, 20, 26, 31, 40, 51, 55, 63, 74, 81] find 31
-# [7, 20, 26, 31, 40, 51, 55, 63, 74, 81] find 77
-# ["Alpha", "Beta", "Delta", "Epsilon", "Gamma", "Theta", "Zeta"]find "Delta"
+# LINEAR SEARCH
+# def bnrySearch(list, search):
+# 	listLen = len(list)
+# 	for i in range(listLen): # instead of for loop use while loop
+# 		if search == list[i]:
+# 			return True
+# 	return False
+
+# print(bnrySearch(l1, 310)) # True
+# print(bnrySearch(l2, 80)) # False
+# print(bnrySearch(l3, "Lettuce")) # True
+
 
 def bnrySearch(list, search):
-	listLen = len(list)
-	for i in range(listLen): # instead of for loop use while loop
-		if search == list[i]:
+	start = 0
+	med = 0
+	listLen = len(list) - 1
+
+	while start <= listLen:
+		med = (listLen + start) // 2
+		if list[med] < search:
+			start = med + 1
+
+		elif list[med] > search:
+			listLen = med - 1
+
+		else:
 			return True
+
 	return False
 
-l1 = ["Jeans", 22, 86, 310, "Flour", 48, "Lotto", 163, 7271]
+
+l1 = [22, 86, 310, 48,  163, 7271]
 l2 = [47, 120, 246, 3, 400, 52, 25, 99, 153, 81]
 l3 = ["Hay", "Forage Food", "Wipes", "Lettuce", "Parsley", "Water", "Vitamins"]
 
